1e2d344a9b9a0151d97f53760e3b2674bf6d6181
SkazuHD <81975854+SkazuHD@users.noreply.github.com>
2022-09-29 20:17:01 +0200

    added service-worker

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 //Service Worker
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/js/service-worker.js", {
+    .register("service-worker.js", {
       scope: ".", // <--- THIS BIT IS REQUIRED
     })
     .then(
--- src/js/service-worker.js
+++
@@ -1,5 +0,0 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js");
-
-workbox.loadModule("workbox-strategies");
-
-workbox.routing.registerRoute(({ request }) => request.destination === "image", new workbox.strategies.NetworkFirst());
