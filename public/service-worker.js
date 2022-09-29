70b0849d8a2f10a19a64c904147ec02c5ed10d4c
SkazuHD <81975854+SkazuHD@users.noreply.github.com>
2022-09-29 23:28:49 +0200

    images

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js");
+workbox.loadModule("workbox-strategies");
+
+workbox.routing.registerRoute(({ request }) => request.destination === "style", new workbox.strategies.CacheFirst());
+
+workbox.routing.registerRoute(({ request }) => request.destination === "image", new workbox.strategies.CacheFirst());
+
+workbox.routing.registerRoute(({ request }) => request.destination === "favicon", new workbox.strategies.CacheFirst());
+
+workbox.routing.registerRoute(({ request }) => request.destination === "manifest", new workbox.strategies.CacheFirst());
+
+workbox.routing.registerRoute(({ request }) => request.destination === "script", new workbox.strategies.CacheFirst());
