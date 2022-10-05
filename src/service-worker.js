638fd298e99560b917041ecc38b7ad9e37ecbec5
SkazuHD <81975854+SkazuHD@users.noreply.github.com>
2022-10-05 20:18:54 +0200

    404

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js");
 workbox.loadModule("workbox-strategies");
 
-workbox.routing.registerRoute(({ request }) => request.destination === "style", new workbox.strategies.CacheFirst());
+workbox.routing.registerRoute(({ request }) => request.destination === "style", new workbox.strategies.NetworkFirst());
 
 workbox.routing.registerRoute(({ request }) => request.destination === "image", new workbox.strategies.CacheFirst());
 
@@ -9,4 +9,4 @@ workbox.routing.registerRoute(({ request }) => request.destination === "favicon"
 
 workbox.routing.registerRoute(({ request }) => request.destination === "manifest", new workbox.strategies.CacheFirst());
 
-workbox.routing.registerRoute(({ request }) => request.destination === "script", new workbox.strategies.CacheFirst());
+workbox.routing.registerRoute(({ request }) => request.destination === "script", new workbox.strategies.NetworkFirst());
